3beb1959d74fe5cbbf75caf95db3389183a0b4dd
shouhu1993 <1058018892@qq.com>
2015-12-01 11:39:16 +0800

    fix twrp time display

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,7 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 # Video
 TARGET_HAVE_SIGNED_VENUS_FW := true
 
